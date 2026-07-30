bc083e961bc5b25040f998339460f2ea4e880a7c
Nakkshh <nakshtrajain25@gmail.com>
2026-07-30 20:46:50 +0530

    Array 001: Find Largest Element

---
+++ src/arrays/Array001_LargestElement.java
@@ -0,0 +1,31 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class Array001_LargestElement {
+    public static int largestElement(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        for(int i = 0 ; i < nums.length ; i++){
+            if(nums[i] > max){
+                max = nums[i];
+            }
+            else if(nums[i] == max){
+                max = max;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int [] arr = new int[5];
+        for(int i = 0 ; i < arr.length ; i++){
+            System.out.print("Enter element : ");
+            arr[i] = sc.nextInt();
+        }
+        int result = largestElement(arr);
+        System.out.println("Max : " + result);
+    }
+}
