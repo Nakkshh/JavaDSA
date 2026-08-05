c7d31a17955c779077054557024661fed51a5bf8
Nakkshh <nakshtrajain25@gmail.com>
2026-08-05 20:21:48 +0530

    Array 003: Move Zeroes

---
+++ src/arrays/Array003_MoveZeroes.java
@@ -0,0 +1,17 @@
+package arrays;
+
+public class Array003_MoveZeroes {
+    public void moveZeroes(int [] arr){
+        int i = 0;
+        for(int j = 0 ; j < arr.length ; j++){
+            if(arr[j] != 0){
+                arr[i] = arr[j];
+                i++;
+            }
+        }
+        while(i < arr.length){
+            arr[i] = 0;
+            i++;
+        }
+    }
+}
