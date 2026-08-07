ba1d33ee195b04267991d5f3037bd9346a235880
Nakkshh <nakshtrajain25@gmail.com>
2026-08-07 22:48:29 +0530

    Array 007: Check if Array is Sorted

---
+++ src/arrays/Array007_CheckIfArrayIsSorted.java
@@ -0,0 +1,12 @@
+package arrays;
+
+public class Array007_CheckIfArrayIsSorted {
+    public boolean isSorted(int [] arr){
+        for(int i = 0 ; i < arr.length-1 ; i++){
+            if(arr[i] > arr[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
