045c657bcdf992f71e2bcfda2e5d672e8eb939f2
Nakkshh <nakshtrajain25@gmail.com>
2026-08-10 23:40:08 +0530

    Array 012: Majority Element

---
+++ src/arrays/Array012_MajorityElement.java
@@ -0,0 +1,22 @@
+package arrays;
+
+public class Array012_MajorityElement {
+    public int majorityElement(int [] nums){
+        int candidate = 0;
+        int count = 0;
+
+        for(int num : nums){
+            if(count == 0){
+                candidate = num;
+            }
+
+            if(num == candidate){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return candidate;
+    }
+}
