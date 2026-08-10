7eb8b416e5d08f46edf8da4b38ad19edaa7593b1
Nakkshh <nakshtrajain25@gmail.com>
2026-08-10 23:57:11 +0530

    Array 013: Maximum Subarray (Kadane's Algorithm)

--- src/arrays/Array013_MaximumSubarray.java
+++ src/arrays/Array013_MaximumSubarray.java
@@ -5,7 +5,7 @@ public class Array013_MaximumSubarray {
         int currentSum = nums[0];
         int maxSum = nums[0];
 
-        for(int i = 0 ; i < nums.length ; i++){
+        for(int i = 1 ; i < nums.length ; i++){
             currentSum = Math.max(nums[i] , currentSum + nums[i]);
             maxSum = Math.max(maxSum,currentSum);
         }
