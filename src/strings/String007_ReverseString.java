47d3e095a5e4a62d8b15a100891088425be1439d
Nakkshh <nakshtrajain25@gmail.com>
2026-08-12 18:39:12 +0530

    String 007: Reverse String

---
+++ src/strings/String007_ReverseString.java
@@ -0,0 +1,14 @@
+package strings;
+
+public class String007_ReverseString {
+    public static void main(String[] args){
+      String s = "Prgramming";
+      StringBuilder sb = new StringBuilder();
+
+      for(int i = s.length()-1 ; i >= 0 ; i--){
+          sb.append(s.charAt(i));
+      }
+
+        System.out.println(sb);
+    }
+}
